90832d56f9ef30b27399b0a98237207e31ec1b0e
Malcolm-Mac <malcolmprojects12@gmail.com>
2024-11-10 04:29:35 +0200

    added: communication with Discount.Grpc and calculating latest prices of products

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -19,18 +19,19 @@ public class StoreBasketCommandHandler(
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        //TODO : communicate with Discount.Grpc and calculate latest prices of products
-
         await DeductDiscount(command.Cart, cancellationToken);
-
         await repository.StoreBasket(command.Cart, cancellationToken);
         return new StoreBasketResult(command.Cart.Username);
     }
 
-    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken){
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
         foreach (var item in cart.Items)
         {
-            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest{ProductName = item.ProductName});
+            var coupon = await discountProto.GetDiscountAsync(
+                new GetDiscountRequest { ProductName = item.ProductName },
+                cancellationToken: cancellationToken
+                );
             item.Price -= coupon.Amount;
         }
     }
